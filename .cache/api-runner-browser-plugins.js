289bbfd2ea0e3701dce90727b2a79a21542db490
bfgonzalez <bfgonzalez142@gmail.com>
2019-08-03 09:00:33 +0800

    home(feat): add hero section

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [{
+      plugin: require('/Users/biancagonzalez/Desktop/side-projects/portfolio/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Bianca Gonzalez`,
+    description: `Hi there, I'm Bianca! I'm a self-taught Front-End Web Developer and User Interface (UI) Designer.`,
+    author: `@bfgonzalez`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
