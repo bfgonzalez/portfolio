39a8b75b69452a1d40e4f82dfe14a5602bcffef7
bfgonzalez <bfgonzalez142@gmail.com>
2019-08-03 19:31:30 +0800

    home(fix): fix contact links

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,6 +8,6 @@ exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/biancagonzalez/Desktop/side-projects/portfolio/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/biancagonzalez/Desktop/side-projects/portfolio/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/biancagonzalez/Desktop/side-projects/portfolio/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/biancagonzalez/Desktop/side-projects/portfolio/src/pages/page-2.js")))
+  "component---src-pages-blog-js": hot(preferDefault(require("/Users/biancagonzalez/Desktop/side-projects/portfolio/src/pages/blog.js")))
 }
 
