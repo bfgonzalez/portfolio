17747f8e9a71b1cafdad5c865f1c813d3278d70a
bfgonzalez <bfgonzalez142@gmail.com>
2020-08-19 13:57:59 +0800

    work(feat): add project details
    add project title and description, add tags for tech stack used, add button to view live demo, create tags component

--- src/components/Work/ProjectTiles.js
+++ src/components/Work/ProjectTiles.js
@@ -1,46 +1,73 @@
 import React from "react"
+
+import Tags from "../Tags/Tags"
+
 import ampWebsiteThumbnail from "../../assets/amp-website.png"
 import uxsocWebsiteThumbnail from "../../assets/uxsoc-website.png"
 
-
 const projects = [
-  {
-    "url": "https://www.uxsociety.org/",
-    "imageSource": uxsocWebsiteThumbnail,
-    "altText": "User Experience Society (Official Website)"
-  },
-  {
-    "url": "https://bfgonzalez.github.io/amp-website",
-    "imageSource": ampWebsiteThumbnail,
-    "altText": "Ateneo Musicians' Pool (Mock Redesign)"
-
-  }
+	{
+		projectTitle: "User Experience Society (Official Website)",
+		url: "https://www.uxsociety.org/",
+		projectDescription:
+			"The official website of User Experience Society—a student organization that advocates and practices human-centered design in solving problems in the community.",
+		imageSource: uxsocWebsiteThumbnail,
+		altText: "User Experience Society (Official Website)",
+		techStack: ["React.js"],
+	},
+	{
+		projectTitle: "Ateneo Musicians' Pool Website (Mock Redesign)",
+		url: "https://bfgonzalez.github.io/amp-website",
+		projectDescription:
+			"A mock redesign of the Ateneo Musicians' Pool website. The Ateneo Musicians' Pool (AMP) is a non-profit organization where individuals realize their holistic formation through music.",
+		imageSource: ampWebsiteThumbnail,
+		altText: "Ateneo Musicians' Pool (Mock Redesign)",
+		techStack: ["HTML", "CSS", "JavsScript"],
+	},
 ]
 
 const ProjectTile = ({ content }) => {
-  let {url, imageSource, altText} = content;
+	let {
+		projectTitle,
+		url,
+		projectDescription,
+		imageSource,
+		altText,
+		techStack,
+	} = content
 
-  return(
-    <div>
-      <a href={url} target="_blank" rel="noopener noreferrer">
-        <div className="image">
-          <img src={imageSource} alt={altText}/>
-        </div>
-      </a>
-    </div>
-  )
+	return (
+		<div className="columns">
+			<div className="column is-6 mb-2">
+				<div className="image">
+					<img src={imageSource} alt={altText} />
+				</div>
+			</div>
+			<div className="column is-6 mb-4">
+				<h2 className="is-size-4 has-text-weight-bold">{projectTitle}</h2>
+				<p className=" py-1">{projectDescription}</p>
+				<Tags labels={techStack} />
+				<a href={url} target="_blank" rel="noopener noreferrer">
+					<button className="button is-primary has-text-weight-bold mt-1">
+						View Demo
+					</button>
+				</a>
+			</div>
+			<hr />
+		</div>
+	)
 }
 
 const ProjectTiles = () => {
-  return(
-    <div className="columns">
-      {projects.map((project, index) => (
-        <div className="column is-6">
-          <ProjectTile key={index} content={project}/>
-        </div>
-      ))}
-    </div>
-  )
+	return (
+		<div>
+			{projects.map((project, index) => (
+				<div>
+					<ProjectTile key={index} content={project} />
+				</div>
+			))}
+		</div>
+	)
 }
 
 export default ProjectTiles
